c3a5b2936ef5aab033f1a3f6a24d71c491edeb3d
juanjoseArangom <jarangoma@unal.edu.co>
2024-11-22 21:18:59 -0500

    YA

--- Taller 3/punto3.py
+++ Taller 3/punto3.py
@@ -7,12 +7,15 @@ while True:
         if entrada[0].isdigit():
             entrada = int(entrada[0])
             listica.append(entrada)
-        elif entrada[0] == "C" or entrada[0] == "D" and entrada[1] == "1":
-            listica.pop(-1)
+        elif entrada[0] == "C" or (entrada[0] == "D" and entrada[1] == "1"):
+            listica = listica[:-1]
         elif entrada[0] == "D" and entrada[1].isdigit():
-            if int(entrada[1]) < len(listica):
+            if int(entrada[1]) <= len(listica) and int(entrada[1]) >= 0:
                 listica = listica[:-int(entrada[1])]
-    elif len(entrada) >= 3 and entrada[1].isdigit() and entrada[2].isdigit():
+    elif len(entrada) == 3 and entrada[1].isdigit() and entrada[2].isdigit():
         n, m = int(entrada[1]), int(entrada[2])
-        print(" ".join(map(str, listica[n-1:m])))
-        
+        if n > len(listica) or m > len(listica):
+            pass
+        else:
+            print(*listica[n-1:m], sep="")
+
